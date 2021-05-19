10832bce651d262275c1a3a154b5963ea70a36cf
Antonio <antonio.loncar@kickstage.com>
2021-05-19 22:55:11 +0200

    ping pong action

---
+++ src/commands/ping.ts
@@ -0,0 +1,10 @@
+import { Message } from 'discord.js';
+
+export default {
+    name: 'ping',
+    description: 'Ping!',
+    execute: (msg: Message, args) => {
+        msg.reply('pong');
+        msg.channel.send('pong');
+    }
+};
