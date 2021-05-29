b5ce9781a05d696da278214d1d74c6db4ec06de3
Antonio <antonio.loncar@kickstage.com>
2021-05-29 23:27:07 +0200

    added args check

--- src/commands/publish/index.ts
+++ src/commands/publish/index.ts
@@ -1,5 +1,5 @@
-import { Message, MessageEmbed } from 'discord.js';
-import * as FacebookService from '../services/facebook_service';
+import { Message } from 'discord.js';
+import ArgsFunction from './args';
 
 export default {
     name: '!publish',
@@ -34,32 +34,39 @@ export default {
                     }
                 })
 
-                if(isPublished) {
+                if (isPublished) {
                     message.reply('Content already published.')
-                    return 
+                    return
                 }
 
                 if (likes < response.likes) return;
 
                 response.likes = likes;
 
-                if(message.attachments.size > 0){
+                if (message.attachments.size > 0) {
                     message.attachments.each(attachment => {
                         response.url = attachment.url;
                     })
-                } else if(message.embeds.length > 0) {
+                } else if (message.embeds.length > 0) {
                     message.embeds.forEach(messageEmbed => {
                         response.url = messageEmbed.url;
                     })
                 }
-            
+
                 response.description = message.content;
 
                 message.react('📨');
                 msg.channel.send(`Likes ${response.likes}, description: ${response.description} ${response.url}`);
-                
-                //publish image to fb
-                FacebookService.publish(response.url, response.description);
+
+                args.forEach((element, index) => {
+                    if (element === '--desc') {
+                        if (args.length >= index) {
+                            response.description = args[index + 1] ? args[index + 1] : response.description;
+                        }
+                    } else if (element === 'ig' || element === 'fb') {
+                        ArgsFunction[element].publish(msg.author.id, response.url, response.description);
+                    }
+                });
             });
     }
 };
